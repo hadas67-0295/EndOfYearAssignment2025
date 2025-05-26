b26535dff2cc102f11955e0ce54c3ef0e990d87b
hadas <hadasabu67@gmail.com>
2025-05-26 20:09:40 +0300

    finish

--- app/src/main/java/com/example/endofyearassignment2025/MainActivity2.java
+++ app/src/main/java/com/example/endofyearassignment2025/MainActivity2.java
@@ -2,6 +2,7 @@ package com.example.endofyearassignment2025;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -52,7 +53,8 @@ public class MainActivity2 extends AppCompatActivity {
         btnEqual = findViewById(R.id.btnEqual);
         btnShowScore = findViewById(R.id.btnShowScore);
 
-        userName = getIntent().getStringExtra("userName");
+        SharedPreferences sharedPreferences = getSharedPreferences("UserData",MODE_PRIVATE);
+        userName = sharedPreferences.getString("username","GuestName");
         tvUserName.setText("Hello " + userName);
 
         questionList = populateQuestions(7);
